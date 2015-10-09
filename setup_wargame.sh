7e5116ab10f8f037a1c0626a7d3d65593bdaacad
Avi Weinstock <aweinstock314@gmail.com>
2015-10-09 13:25:20 -0400

    Add a warning message to setup_wargame.sh (it looks like someone's actually made this mistake in IRC recently, but it's probably a good idea to have a warning even if it turns out that they're ok).

--- setup_wargame.sh
+++ setup_wargame.sh
@@ -3,10 +3,19 @@
 #########################################
 #          Pre-install checks
 #########################################
+
+# Warn the user about running this on their host machine
+# (because it makes various invasive settings-changes, and runs a ton of vulnerable services)
+echo -e "\x1b[31;1mDON'T RUN THIS ON YOUR REAL COMPUTER\x1b[0m"
+echo -e "If you're running this on your real computer, \x1b[31;1mpress control-c NOW\x1b[0m to stop this script."
+sleep 10
+
 if [ "$USER" != 'root' ]; then
     echo "ERROR: Script must be run using root!"
     exit
 fi
+
+echo -e "\x1b[32;1mProceeding with the installation.\x1b[0m"
 
 #########################################
 #     Per-Machine Install Variables
